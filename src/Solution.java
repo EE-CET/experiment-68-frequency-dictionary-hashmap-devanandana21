556bb845f95b0277c873ddfde4d8079b07d01bcf
devanandana21 <tve24el018@cet.ac.in>
2026-02-27 10:55:12 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,15 @@
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String line = scanner.nextLine().trim();
-
         String[] words = line.split("\\s+");
 
-        HashMap<String, Integer> frequencyMap = new HashMap<>();
+        TreeMap<String, Integer> frequencyMap = new TreeMap<>();
 
         for (String word : words) {
             frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
